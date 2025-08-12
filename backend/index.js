36a4f734871c15190525291c122bf8af0722d08a
gabgustavo <gustavoavilabar@hotmail.com>
2025-08-12 11:30:53 -0500

    conexion a base de datos

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,28 @@
 let express = require("express");
+let mongoose = require("mongoose");
 
 let app = express();
 let port = process.env.PORT || 3000;
+let portDB = process.env.PORT_DB || 27017;
+let hostDB = process.env.HOST_DB || '//localhost';
+let DBName = process.env.DB_NAME || 'tienda';
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(`mongodb:${hostDB}:${portDB}/${DBName}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log('Connected to MongoDB successfully!');
+  } catch (error) {
+    console.error('MongoDB connection error: ', error);
+  }
+}
+connectToDatabase();
 
 app.listen(port, () => { 
-    console.log(`Server is running on port: ${port}`);
-    
+    console.log(`Server is running on port: ${port}`);    
 });
+
 
 module.export = app;
