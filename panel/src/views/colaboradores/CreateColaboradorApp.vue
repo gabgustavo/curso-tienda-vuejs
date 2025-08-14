60d149d2bcc03e3430f18a89a059ea1d3d556355
gabgustavo <gustavoavilabar@hotmail.com>
2025-08-14 16:22:14 -0500

    agregar componente topnav

--- panel/src/views/colaboradores/CreateColaboradorApp.vue
+++ panel/src/views/colaboradores/CreateColaboradorApp.vue
@@ -3,40 +3,7 @@
         <Sidebar />
         <div class="main-content">
 
-            <nav class="navbar navbar-expand-md navbar-light d-md-flex">
-                <div class="container-fluid">
-
-                <!-- Form -->
-                <form class="form-inline me-4 d-none d-md-flex">
-                    
-                </form>
-
-                <!-- User -->
-                <div class="navbar-user">
-
-
-                    <!-- Dropdown -->
-                    <div class="dropdown">
-
-                    <!-- Toggle -->
-                    <a href="#" class="avatar avatar-sm avatar-online dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="/assets/img/avatar-1.jpg" alt="..." class="avatar-img rounded-circle" style="-webkit-mask-image: none;">
-                    </a>
-
-                    <!-- Menu -->
-                    <div class="dropdown-menu dropdown-menu-end">
-                        <a href="./profile-posts.html" class="dropdown-item">Profile</a>
-                        <a href="./account-general.html" class="dropdown-item">Settings</a>
-                        <hr class="dropdown-divider">
-                        <a href="./sign-in.html" class="dropdown-item">Logout</a>
-                    </div>
-
-                    </div>
-
-                </div>
-
-                </div>
-            </nav>
+            <TopNav />
 
             <div class="container-fluid">
                 <div class="row justify-content-center">
@@ -194,11 +161,13 @@
 <script>
 // @ is an alias to /src
 import Sidebar from '@/components/SideBar.vue';
+import TopNav from '@/components/TopNav.vue';
 
 export default {
   name: 'CreateColaboradorApp',
   components: {
-      Sidebar
+      Sidebar,
+      TopNav
   }
 }
 </script>
